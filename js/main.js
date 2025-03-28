272574e9abcd0874782e4825a41aee3f85bf0496
nikgav1 <nikgav66@gmail.com>
2025-03-28 19:30:13 +0300

    Added Class to create sketch and css to style all of it

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import Sketch from "./sketch.js"
+
+const sketch = new Sketch(960, 16, "container")
+sketch.createGrid()
---
+++ js/sketch.js
@@ -0,0 +1,19 @@
+ export default class Sketch{
+    constructor(sketchSize, gridSize, container){
+        this.sketchSize = sketchSize;
+        this.gridSize = gridSize;
+        this.container = container;
+    }
+    createGrid(){
+        const gridPlaceHolder = document.getElementById(this.container)
+        const cellSize = this.sketchSize / this.gridSize
+        const numberOfCells = Math.pow(this.gridSize, 2) - this.gridSize
+        for(let i = 0; i < numberOfCells; i++){
+            const cell = document.createElement("div")
+            cell.style.width = `${cellSize}px`
+            cell.style.height = `${cellSize}px`
+            cell.classList.add("gridElement")
+            gridPlaceHolder.appendChild(cell);
+        }
+    }
+}
