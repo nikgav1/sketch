007ac482ab38d41a2196c099f02d4d7408b3fe47
nikgav1 <nikgav66@gmail.com>
2025-03-28 19:50:00 +0300

    Fixed bug where the last column would not tuen black

--- js/sketch.js
+++ js/sketch.js
@@ -6,13 +6,22 @@
     }
     createGrid(){
         const gridPlaceHolder = document.getElementById(this.container)
+
+        gridPlaceHolder.style.display = "flex";
+        gridPlaceHolder.style.flexWrap = "wrap";
+        gridPlaceHolder.style.width = `${this.sketchSize}px`;
+        gridPlaceHolder.style.height = `${this.sketchSize}px`;
+
         const cellSize = this.sketchSize / this.gridSize
-        const numberOfCells = Math.pow(this.gridSize, 2) - this.gridSize
+        const numberOfCells = Math.pow(this.gridSize, 2)
+
         for(let i = 0; i < numberOfCells; i++){
             const cell = document.createElement("div")
             cell.style.width = `${cellSize}px`
             cell.style.height = `${cellSize}px`
             cell.classList.add("gridElement")
+
+            // Add hover effect to turn the cell black
             cell.addEventListener("mouseover", () =>{
                 cell.style.backgroundColor = "black"
             })
