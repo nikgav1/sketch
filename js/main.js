64d4996329eb0e21dedea19b3e10a63b850da7d2
nikgav1 <nikgav66@gmail.com>
2025-03-29 10:58:08 +0300

    Formatted with Prettier

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-import Sketch from "./sketch.js"
+import Sketch from "./sketch.js";
 
 // Default settings
-let sketchSize = 960
-let gridSize = 16
+let sketchSize = 960;
+let gridSize = 16;
 
 // Create and initialize grid
-let sketch = new Sketch(sketchSize, gridSize, "container")
-sketch.createGrid()
+let sketch = new Sketch(sketchSize, gridSize, "container");
+sketch.createGrid();
 
-const changeSkecthButton = document.getElementById("changeSkecthButton")
-const numberInput = document.getElementById("number-input")
+const changeSkecthButton = document.getElementById("changeSkecthButton");
+const numberInput = document.getElementById("number-input");
 
 changeSkecthButton.addEventListener("click", () => {
-    const number = numberInput.value;
-    if(number < 2 || number > 100){
-        alert("Enter Valid Number")
-    }
-    else{
-        sketch.removeGrid();
-        sketch = new Sketch(sketchSize, number, "container");
-        sketch.createGrid();
-    }
-})
+  const number = numberInput.value;
+  if (number < 2 || number > 100) {
+    alert("Enter Valid Number");
+  } else {
+    sketch.removeGrid();
+    sketch = new Sketch(sketchSize, number, "container");
+    sketch.createGrid();
+  }
+});
--- js/sketch.js
+++ js/sketch.js
@@ -1,46 +1,51 @@
- export default class Sketch{
-    constructor(sketchSize, gridSize, container){
-        this.sketchSize = sketchSize;
-        this.gridSize = gridSize;
-        this.container = container;
-    }
-    createGrid(){
-        // Initializes the grid container with Flexbox
-        const gridPlaceHolder = document.getElementById(this.container)
+export default class Sketch {
+  constructor(sketchSize, gridSize, container) {
+    this.sketchSize = sketchSize;
+    this.gridSize = gridSize;
+    this.container = container;
+  }
+  createGrid() {
+    // Initializes the grid container with Flexbox
+    const gridPlaceHolder = document.getElementById(this.container);
 
-        gridPlaceHolder.style.display = "flex";
-        gridPlaceHolder.style.flexWrap = "wrap";
-        gridPlaceHolder.style.width = `${this.sketchSize}px`;
-        gridPlaceHolder.style.height = `${this.sketchSize}px`;
+    gridPlaceHolder.style.display = "flex";
+    gridPlaceHolder.style.flexWrap = "wrap";
+    gridPlaceHolder.style.width = `${this.sketchSize}px`;
+    gridPlaceHolder.style.height = `${this.sketchSize}px`;
 
-        const cellSize = this.sketchSize / this.gridSize // Size of each cell
-        const numberOfCells = Math.pow(this.gridSize, 2) // Total number of cells
-        
-        // Creates the entire grid
-        for(let i = 0; i < numberOfCells; i++){
-            // Creates a single grid cell with hover effect
-            const cell = document.createElement("div")
-            cell.style.width = `${cellSize}px`
-            cell.style.height = `${cellSize}px`
-            cell.classList.add("gridElement")
+    const cellSize = this.sketchSize / this.gridSize; // Size of each cell
+    const numberOfCells = Math.pow(this.gridSize, 2); // Total number of cells
 
-            // Add hover effect to turn the cell black
-            cell.addEventListener("mouseover", () =>{
-                const newRgb = this.#randomRGB()
-                cell.style.backgroundColor = `rgb(${newRgb[0]}, ${newRgb[1]}, ${newRgb[2]})`
-            })
-            gridPlaceHolder.appendChild(cell);
-        }
+    // Creates the entire grid
+    for (let i = 0; i < numberOfCells; i++) {
+      // Creates a single grid cell with hover effect
+      const cell = document.createElement("div");
+      cell.style.width = `${cellSize}px`;
+      cell.style.height = `${cellSize}px`;
+      cell.classList.add("gridElement");
+
+      // Add hover effect to turn the cell black
+      cell.addEventListener("mouseover", () => {
+        const newRgb = this.#randomRGB();
+        cell.style.backgroundColor = `rgb(${newRgb[0]}, ${newRgb[1]}, ${newRgb[2]})`;
+      });
+      gridPlaceHolder.appendChild(cell);
     }
-    removeGrid(){
-        // Iterate through every cell and delete it
-        const gridPlaceHolder = document.getElementById(this.container)
-        while (gridPlaceHolder.firstChild) {
-            gridPlaceHolder.removeChild(gridPlaceHolder.lastChild);
-          }
+  }
+  removeGrid() {
+    // Iterate through every cell and delete it
+    const gridPlaceHolder = document.getElementById(this.container);
+    while (gridPlaceHolder.firstChild) {
+      gridPlaceHolder.removeChild(gridPlaceHolder.lastChild);
     }
-    #randomRGB(){
-        const rgb = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)]
-        return rgb
-    }
+  }
+  #randomRGB() {
+    // Random rgb is getting created
+    const rgb = [
+      Math.floor(Math.random() * 256),
+      Math.floor(Math.random() * 256),
+      Math.floor(Math.random() * 256),
+    ];
+    return rgb;
+  }
 }
